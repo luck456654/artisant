f348fb9de00ca1d4c5a63335a8f845e073ff9eac
Vadim <luckjantsev@rambler.ru>
2022-04-07 17:50:31 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import axios from "axios";
 import React, {useState, useEffect} from "react";
-function App() {
-  const [dataapi, setData] = useState([]);
+
+
+  
+
+  
+function App(withdrawRemainder) {
+  let available=0;
+  const [dataapi, setData] = useState([]); 
+   
   useEffect(() => {
+    console.log("257");
   const url = "https://artisant.io/api/products";
     axios
     .get(
@@ -15,20 +23,44 @@ function App() {
       }
     )
     
-      .then(res => {
-        setData(res);
-       })
-      .then(console.log(dataapi.data.data.products[0].name))  
-     
-    
-    },[])
-  return (
-    <div className="App">
-      {dataapi.map((item) => (
-        <div>{item.data.data.products[0].name}</div>
-      ))}
-    </div>
-  );
-}
-
+    .then(res => {
+      setData(res.data.data.products);
+     })
+    })
+  
+    function withdrawRemainder(){
+      console.log(123)
+           return setData(dataapi.filter(item => item.quantity_available >1))
+           }
+  
+    return (
+        <div className="App">
+          <h1 className='content__title'>Explore</h1>
+            <p>Buy and sell digital fashion NFT art</p>
+            <input type="checkbox" onChange={withdrawRemainder}/>Вывести только товары которых остаток больше ноля
+          <div className='content'>
+          {dataapi.map((item) => (
+            <div className='content__card'>
+              <div className='content__imgProduct'>
+                <div className='content__titleProduct'>
+                {item.name}
+                </div>
+                </div>
+              <div className='content__info'>
+              <div>  
+              available
+              <p><b>1 of {item.quantity_available}</b></p>
+              </div>
+              <div className='content__price'>
+              <div class="content__price-title">  
+              price
+              <p class="content__price">{item.initial_price}ETH</p>
+              </div>
+              </div>
+              </div>
+              </div>
+          ))}
+        </div>
+        </div>
+    )}
 export default App;
